30b994d81ade9f1db55ebfc8a00c67f46c1d5971
faimi <51162351+faimi@users.noreply.github.com>
2020-01-10 22:48:50 +0800

    仿大众点评

---
+++ src/components/FoodListDetail/foodListDetail.vue
@@ -0,0 +1,15 @@
+<template>
+<van-row>
+  <van-col span="8">span: 8</van-col>
+  <van-col span="8">span: 8</van-col>
+  <van-col span="8">span: 8</van-col>
+</van-row>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="less" scoped>
+
+</style>
--- src/components/Swipe/swipe.vue
+++ src/components/Swipe/swipe.vue
@@ -1,26 +1,21 @@
 <template>
-  <!-- <div class="swiper-container">
+  <div class="swiper-container">
     <div class="swiper-wrapper">
       <div class="swiper-slide" style="margin-top: 12px;">
-        <van-row>
+        <div class="row">
           <div v-for="(index,key) in name" :key="key">
-            <van-col span="1"></van-col>
-            <van-col span="2">
+            <div class="col-md-1"></div>
+            <div class="col-md-2 col-md-2-style">
               <img class="img" :src="index.pict" @click="$router.push('/deliciousFood')" />
               <div class="font">{{index.tit}}</div>
-            </van-col>
+            </div>
           </div>
-        <van-row>
+        </div>
       </div>
-  </div>-->
-  <!-- 如果需要分页器 -->
-  <!-- <div class="swiper-pagination"></div>
-  </div>-->
-<van-row>
-  <van-col span="2">span: 8</van-col>
-  <van-col span="3">span: 8</van-col>
-  <van-col span="3">span: 8</van-col>
-</van-row>
+    </div>
+    <!-- 如果需要分页器 -->
+    <div class="swiper-pagination"></div>
+  </div>
 </template>
 
 <script>

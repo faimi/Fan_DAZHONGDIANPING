50f11ad0753e4ea11645f0b4f5d07af98efc2f14
faimi <51162351+faimi@users.noreply.github.com>
2021-01-14 20:57:05 +0800

    更新

--- src/pages/Home/home.vue
+++ src/pages/Home/home.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
+    <van-dropdown-menu>
+      <van-dropdown-item v-model="value1" :options="option1" />
+      <van-dropdown-item v-model="value2" :options="option2" />
+    </van-dropdown-menu>
     <v-Search></v-Search>
     <v-Swipe></v-Swipe>
     <div class="resBorder"></div>
-    <v-GuessLike></v-GuessLike> 
+    <v-GuessLike></v-GuessLike>
   </div>
 </template>
 <script>
@@ -15,13 +19,29 @@ export default {
     "v-Swipe": Swipe,
     "v-GuessLike": GuessLike,
     "v-Search": Search
+  },
+  data() {
+    return {
+      value1: 0,
+      value2: "a",
+      option1: [
+        { text: "所有商品", value: 0 },
+        { text: "新款商品", value: 1 },
+        { text: "活动商品", value: 2 }
+      ],
+      option2: [
+        { text: "默认排序", value: "a" },
+        { text: "好评排序", value: "b" },
+        { text: "销量排序", value: "c" }
+      ]
+    };
   }
 };
 </script>
 <style lang="less">
-  .resBorder {
-    background-color: #f0f0f0;
-    height: 20px;
-    width: 100%;
-  }
+.resBorder {
+  background-color: #f0f0f0;
+  height: 20px;
+  width: 100%;
+}
 </style>
